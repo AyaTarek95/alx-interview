df5effb404d3cc74cfea53d89e09c784593b4e2f
Aya Tarek <132375842+AyaTarek95@users.noreply.github.com>
2024-04-14 20:51:49 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -25,16 +25,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
-    first_row = [1]
-
-    triangle.append(first_row)
+    triangle = [[1]]
 
     for i in range(1, n):
-        row = [1]
-        for j in range(len(triangle[i - 1]) - 1):
-            row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
-        row.append(1)
-        triangle.append(row)
+        next_row = [1]
+        for j in range(1, i):
+            next_row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        next_row.append(1)
+        triangle.append(next_row)
 
     return triangle
