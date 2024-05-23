00ff010fdcc97851c8ecf2871a5e574ac60022fa
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-24 01:28:25 +0300

    f01

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,14 +4,14 @@ const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  order(actors, 0);
+  const characters = JSON.parse(body).characters;
+  print_chars(characters, 0);
 });
-const order = (actors, i) => {
-  if (i === actors.length) return;
-  request(actors[i], function (err, res, body) {
+const print_chars = (characters, i) => {
+  if (i === characters.length) return;
+  request(characters[i], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
-    order(actors, i + 1);
+    print_chars(characters, i + 1);
   });
 };
