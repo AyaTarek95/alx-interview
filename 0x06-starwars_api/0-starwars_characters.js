3e355ba8f5935690efe113a6c724ffea626d11fe
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-24 01:30:40 +0300

    semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,13 +5,13 @@ const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const characters = JSON.parse(body).characters;
-  print_chars(characters, 0);
+  printChars(characters, 0);
 });
-const print_chars = (characters, i) => {
+const printChars = (characters, i) => {
   if (i === characters.length) return;
   request(characters[i], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
-    print_chars(characters, i + 1);
+    printChars(characters, i + 1);
   });
 };
